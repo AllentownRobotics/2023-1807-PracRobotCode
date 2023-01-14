858a1ff88f992b1c4810cc27bdb3ff63e40b6262
hueye <114522370+hueye@users.noreply.github.com>
2023-01-14 15:27:03 -0500

    Add files via upload

---
+++ 2023-ChargedUp/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,55 @@
+package frc.robot;
+
+
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+
+    WPI_TalonFX leftMotorOne;
+    WPI_TalonFX leftMotorTwo;
+    WPI_TalonFX leftMotorThree;
+
+    WPI_TalonFX rightMotorOne;
+    WPI_TalonFX rightMotorTwo;
+    WPI_TalonFX rightMotorThree;
+
+    MotorControllerGroup leftMotors;
+    MotorControllerGroup rightMotors;
+
+    DifferentialDrive differentialDrive;
+
+public Drivetrain() {
+    leftMotorOne = new WPI_TalonFX(Constants.DRIVETRAIN_LEFTMOTOR_ONE);
+    leftMotorTwo = new WPI_TalonFX(Constants.DRIVETRAIN_LEFTMOTOR_TWO);
+    leftMotorThree = new WPI_TalonFX(Constants.DRIVETRAIN_LEFTMOTOR_THREE);
+
+    rightMotorOne = new WPI_TalonFX(Constants.DRIVETRAIN_RIGHTMOTOR_ONE);
+    rightMotorTwo = new WPI_TalonFX(Constants.DRIVETRAIN_RIGHTMOTOR_TWO);
+    rightMotorThree = new WPI_TalonFX(Constants.DRIVETRAIN_RIGHTMOTOR_THREE);
+
+    leftMotors = new MotorControllerGroup(leftMotorOne, leftMotorTwo, leftMotorThree);
+    rightMotors = new MotorControllerGroup(rightMotorOne, rightMotorTwo, rightMotorThree);
+
+    leftMotorOne.setNeutralMode(NeutralMode.Coast);
+    leftMotorTwo.setNeutralMode(NeutralMode.Coast);
+    leftMotorThree.setNeutralMode(NeutralMode.Coast);
+
+    rightMotorOne.setNeutralMode(NeutralMode.Coast);
+    rightMotorTwo.setNeutralMode(NeutralMode.Coast);
+    rightMotorThree.setNeutralMode(NeutralMode.Coast);
+    leftMotors.setInverted(true);
+
+    differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+}
+    public void CurveDrive(double speed, double turnBy, boolean turnInPlace) {
+    differentialDrive.curvatureDrive(speed, turnBy, turnInPlace);
+        }
+    
+}
