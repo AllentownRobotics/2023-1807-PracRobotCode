e4a7afbbd2c69a3c6e77691aa34628aeebc0c457
hueye <114522370+hueye@users.noreply.github.com>
2023-01-14 15:23:03 -0500

    Add files via upload

--- 2023-ChargedUp/src/main/java/frc/robot/Constants.java
+++ 2023-ChargedUp/src/main/java/frc/robot/Constants.java
@@ -1,15 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {}
+public class Constants {
+    public static final int DRIVETRAIN_LEFTMOTOR_ONE = 1;
+
+    public static final int DRIVETRAIN_LEFTMOTOR_TWO = 2;
+ 
+    public static final int DRIVETRAIN_LEFTMOTOR_THREE = 3;
+ 
+    public static final int DRIVETRAIN_RIGHTMOTOR_ONE = 4;
+ 
+    public static final int DRIVETRAIN_RIGHTMOTOR_TWO = 5;
+ 
+    public static final int DRIVETRAIN_RIGHTMOTOR_THREE = 6;
+    
+    public static final int DRIVETRAIN_GYRO_ID = 0; 
+}
