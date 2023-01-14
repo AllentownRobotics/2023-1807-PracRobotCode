9c29bec1891e9823e3849262c7e0bd2246600bfb
Gavin32323 <75314990+Gavin32323@users.noreply.github.com>
2023-01-14 12:31:32 -0500

    moew

--- Gavin_Normand_Redbird_Rewrite/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Gavin_Normand_Redbird_Rewrite/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,23 +4,53 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class FlyWheel extends SubsystemBase {
+public class DriveTrain extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  WPI_TalonFX lead;
-  WPI_TalonFX follow;
+    private WPI_TalonFX leftMotorOne;
+    private WPI_TalonFX leftMotorTwo;
+    private WPI_TalonFX leftMotorThree;
+  
+    private WPI_TalonFX rightMotorFour;
+    private WPI_TalonFX rightMotorFive;
+    private WPI_TalonFX rightMotorSix;
 
-  public FlyWheel() {
-    lead = new WPI_TalonFX(Constants.flyid1);
-    follow = new WPI_TalonFX(Constants.flyid2);
-    follow.follow(lead);
-    lead.setInverted(true);
+    private MotorControllerGroup left;
+    private MotorControllerGroup right;
+
+    private DifferentialDrive drive;
+
+  public DriveTrain() {
+    leftMotorOne = new WPI_TalonFX(Constants.lm1);
+    leftMotorTwo = new WPI_TalonFX(Constants.lm2);
+    leftMotorTwo = new WPI_TalonFX(Constants.lm3);
+    rightMotorFour = new WPI_TalonFX(Constants.rm4);
+    rightMotorFive = new WPI_TalonFX(Constants.rm5);
+    rightMotorSix = new WPI_TalonFX(Constants.rm6);
+    
+    leftMotorOne.setNeutralMode(NeutralMode.Coast);
+    leftMotorTwo.setNeutralMode(NeutralMode.Coast);
+    leftMotorThree.setNeutralMode(NeutralMode.Coast);
+    rightMotorFour.setNeutralMode(NeutralMode.Coast);
+    rightMotorFive.setNeutralMode(NeutralMode.Coast);
+    rightMotorSix.setNeutralMode(NeutralMode.Coast);
+
+    left = new MotorControllerGroup(leftMotorOne, leftMotorTwo, leftMotorThree);
+    right = new MotorControllerGroup(rightMotorFour, rightMotorFive, rightMotorSix);
+
+    right.setInverted(true);
+
+    drive = new DifferentialDrive(left, right);
   }
 
   @Override
@@ -32,8 +62,30 @@ public class FlyWheel extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
-  public void shoot(int rpm)
+  public void drive(XboxController joy)
   {
-    lead.set(TalonFXControlMode.Velocity,rpm);
+    drive.curvatureDrive(joy.getLeftY()*Constants.SpeedMult, joy.getRightX()*Constants.TurnMult, joy.getRightBumper());
+  }
+
+  public void brakeButton(boolean brake) 
+  {
+    if(brake)
+    {
+      leftMotorOne.setNeutralMode(NeutralMode.Brake);
+      leftMotorTwo.setNeutralMode(NeutralMode.Brake);
+      leftMotorThree.setNeutralMode(NeutralMode.Brake);
+      rightMotorFour.setNeutralMode(NeutralMode.Brake);
+      rightMotorFive.setNeutralMode(NeutralMode.Brake);
+      rightMotorSix.setNeutralMode(NeutralMode.Brake);
+    }
+    else
+    {
+      leftMotorOne.setNeutralMode(NeutralMode.Coast);
+      leftMotorTwo.setNeutralMode(NeutralMode.Coast);
+      leftMotorThree.setNeutralMode(NeutralMode.Coast);
+      rightMotorFour.setNeutralMode(NeutralMode.Coast);
+      rightMotorFive.setNeutralMode(NeutralMode.Coast);
+      rightMotorSix.setNeutralMode(NeutralMode.Coast);
+    }
   }
 }
