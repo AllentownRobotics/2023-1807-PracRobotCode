108106fd074b694a75ce39d5ecb92af78ebf09bb
hueye <114522370+hueye@users.noreply.github.com>
2023-01-14 15:24:12 -0500

    Add files via upload

--- 2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
+++ 2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,11 +17,12 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  public static final String driverController = null;
+
+public static Subsystem drivetrain;
+
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+ 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -43,6 +44,21 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
+
+public static Object getY() {
+    return null;
+}
+
+public static Object getButton(int i) {
+    return null;
+}
+
+public static void CurveDrive(Object y, Object y2, Object button) {
+}
+
+public static Object getX() {
+    return null;
+}
 }
