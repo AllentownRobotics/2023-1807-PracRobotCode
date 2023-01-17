412fcdce4e2a56fd0eafea6d7c9ac36100b4b569
Nick <115846324+Atlatl1807@users.noreply.github.com>
2023-01-17 16:22:39 -0500

    this is way better

--- 2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
+++ 2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ColorSensorCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -25,7 +24,6 @@ public class RobotContainer {
   private final ColorSensorSubsystem  m_ColorSensorSubsystem = new ColorSensorSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-  private final ColorSensorCommand m_ColorSensorCommand = new ColorSensorCommand(m_ColorSensorSubsystem);
 
 
   public static ColorSensorV3 m_colorSensor;
@@ -35,7 +33,6 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
 
-    m_ColorSensorSubsystem.setDefaultCommand(m_ColorSensorCommand);
   }
 
   /**
--- 2023-ChargedUp/src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
+++ 2023-ChargedUp/src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -5,10 +5,15 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.ColorSensorV3;
-import frc.robot.subsystems.ColorSensorSubsystem;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 
 
 public class ColorSensorSubsystem extends SubsystemBase {
@@ -19,6 +24,13 @@ public class ColorSensorSubsystem extends SubsystemBase {
   I2C.Port i2cPort;
   ColorSensorV3 m_colorSensor;
 
+  private ColorMatch m_colorMatch;
+  private Color detectedColor;
+  public String colorString = "Null";
+
+  //Colors
+  private final Color PURPLE = new Color(0.6, 0.1, 0.9);
+  private final Color YELLOW = new Color(0.9, 0.9, 0);
 
 
   /** Creates a new ColorSensor. */
@@ -27,6 +39,28 @@ public class ColorSensorSubsystem extends SubsystemBase {
     m_colorSensor = new ColorSensorV3(i2cPort);
 
     System.out.println("Color Sensor Subsystem Initialized");
+  }
+
+  public String ColorDetect() {
+    m_colorMatch.addColorMatch(PURPLE);
+    m_colorMatch.addColorMatch(YELLOW);
+    //Detecting and result 
+    detectedColor = m_colorSensor.getColor();
+
+    ColorMatchResult matchResult = m_colorMatch.matchClosestColor(detectedColor);
+  
+      //Matching Colors 
+    if (matchResult.color.equals(PURPLE)) {
+    colorString = "Purple";
+    } else if (matchResult.color.equals(YELLOW)) {
+    colorString = "Yellow";
+    } else {
+    colorString = "Other";
+    }
+  
+    //Printing all values to the SmartDash
+    SmartDashboard.putString("Color", colorString);
+    return colorString;
   }
 
   
