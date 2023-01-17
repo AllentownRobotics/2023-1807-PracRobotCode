7d8968a32459ba2cad950d2e5a81505eb22a2a09
gavinnormand <75314990+Gavin32323@users.noreply.github.com>
2023-01-17 18:45:21 -0500

    sewerslide

--- GavinRedbirdRewrite2.0/src/main/java/frc/robot/subsystems/FlyWheel.java
+++ GavinRedbirdRewrite2.0/src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
