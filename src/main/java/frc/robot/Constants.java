bd2fefd3aecd5f236778365894351fcc7482b58e
hueye <huey.e.2025@ufrsd.us>
2023-03-16 14:48:47 -0400

    id change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,18 +11,18 @@ public class RobotContainerConstants {
 
 public class CompressorConstants {
     
-    public static final int COMPRESSOR_ID = 2;
+    public static final int COMPRESSOR_ID = 14;
 }
 
 public class ArmConstants {
-// Note 3/15: these may need to be changed in accordance with Reilly's IDs.
-    public static final int LEFT_ARM_EXTEND_CHANNEL_ID = 1;
 
-    public static final int LEFT_ARM_RETRACT_CHANNEL_ID = 2;
+    public static final int LEFT_ARM_EXTEND_CHANNEL_ID = 0;
 
-    public static final int RIGHT_ARM_EXTEND_CHANNEL_ID = 3;
+    public static final int LEFT_ARM_RETRACT_CHANNEL_ID = 1;
 
-    public static final int RIGHT_ARM_RETRACT_CHANNEL_ID = 4;
+    public static final int RIGHT_ARM_EXTEND_CHANNEL_ID = 2;
+
+    public static final int RIGHT_ARM_RETRACT_CHANNEL_ID = 3;
 
 }
 
